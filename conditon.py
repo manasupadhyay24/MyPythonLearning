390a64e3ef6826cf32ed6c19f4a742c20fab7b90
Manasupadhyay <manasupadhyay24092003@gmail.com>
2025-07-20 12:08:55 +0530

    python started

---
+++ conditon.py
@@ -0,0 +1,42 @@
+#  #Traffic light
+
+# light = input("light colour:")
+
+# if (light == "Red"):
+#     print("stop")
+
+# elif (light == "Yellow"):
+#     print("look")
+
+# elif (light == "Green"):
+#     print("go")
+
+# else:
+#     print("light is broken")            
+
+
+# #Grades of students
+
+# marks = input("marks :")
+# if (marks >="90"):
+#     print("A")
+
+# elif (marks >="80" and marks <"90"):
+#     print("B")
+
+# elif (marks >="70" and marks <"80"):
+#     print("C")
+
+# else:
+#     print("D")  
+
+
+
+
+
+
+
+
+
+
+
